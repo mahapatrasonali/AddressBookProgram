fc43ef3a7d4ce7aa9d186c7e4c8bd1d307c9340a
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-02-04 21:49:06 +0530

    Editing Contact

--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@
         ReEnteringApp:
             Console.WriteLine("Select any one from below options \n" +
                 "Press 1 : to Create/Add a new contact\n" +
-                "Press 2 : to view all the contacts in Address book.");
+                "Press 2 : to view all the contacts in Address book\n"+
+                "Press 3:   to Edit Contact\n"+
+                "Press 4:   To Remove Exsiting Contact");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -18,6 +20,12 @@
                     break;
                 case 2:
                     contact.ListPeople();
+                    break;
+                case 3:
+                    contact.EditPerson();
+                    break;
+                case 4:
+                    contact.RemovePerson();
                     break;
                 default:
                     Console.WriteLine("Invalid Option selected , Please try again ");
